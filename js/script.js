7e6de8f35245f83e84bbf2b339a350f8961aee4c
KentondeJong <kenton@kentondejong.travel>
2023-04-11 16:27:17 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const dialog = document.querySelector('dialog')
 
 form.addEventListener('submit', (e) => {
     let name = form.querySelector('input[name="name"').value;
+    name = email.split(' ')[0];
+
     let email = form.querySelector('input[name="email"').value;
     email = email.split('@')[1];
     email = email.split('.')[0];
@@ -10,20 +12,18 @@ form.addEventListener('submit', (e) => {
     dialog.querySelector('h3 span').innerHTML = name;
     dialog.querySelector('p span').innerHTML = email;
 
-    document.body.style.overflow = 'hidden';
-
     form.reset();
     dialog.showModal();
+    
     e.preventDefault();
 })
 
 dialog.querySelector('.close').addEventListener('click', (e) => {
-    document.body.style.overflow = 'auto';
     dialog.close();
 })
 
 dialog.addEventListener('click', (e) => {
-    let rect = e.target.getBoundingClientRect();
+    const rect = e.target.getBoundingClientRect();
     if (rect.left > e.clientX ||
         rect.right < e.clientX ||
         rect.top > e.clientY ||
