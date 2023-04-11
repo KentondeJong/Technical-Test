3d84df45674dcfdf74b10cf6891ebf285267f903
KentondeJong <kenton@kentondejong.travel>
2023-04-11 16:22:42 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,11 +10,25 @@ form.addEventListener('submit', (e) => {
     dialog.querySelector('h3 span').innerHTML = name;
     dialog.querySelector('p span').innerHTML = email;
 
+    document.body.style.overflow = 'hidden';
+
     form.reset();
     dialog.showModal();
     e.preventDefault();
 })
 
 dialog.querySelector('.close').addEventListener('click', (e) => {
+    document.body.style.overflow = 'auto';
     dialog.close();
+})
+
+dialog.addEventListener('click', (e) => {
+    let rect = e.target.getBoundingClientRect();
+    if (rect.left > e.clientX ||
+        rect.right < e.clientX ||
+        rect.top > e.clientY ||
+        rect.bottom < e.clientY
+    ) {
+        dialog.close();
+    }
 })
